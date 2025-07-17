6499fcf0d89e5eae2ef61d4ef36e99fcbd7ed2a7
O'Moncada <oscar.moncada702@gmail.com>
2025-07-17 16:58:52 -0400

    guardar cambios

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,28 +1,24 @@
 'use client';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter  } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { ChangeEvent } from 'react';
+import type { Route } from 'next';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  // function handleSearch(term: string) {
-  const handleSearch = useDebouncedCallback((term) => {
-
-    // console.log(`Searching... ${term}`);
-
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-    replace(`${pathname}?${params.toString()}`);
-  // }
+
+    replace(`${pathname}?${params.toString()}` as Route);
   }, 300);
 
   return (
@@ -33,12 +29,25 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
         defaultValue={searchParams.get('query')?.toString()}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        <svg
+          className="h-4 w-4 stroke-gray-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
